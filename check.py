a0d01561798251ace789e6c2a5f89ff00c8c7cfb
niminim <nimroddaniel1986@gmail.com>
2023-06-26 17:22:27 +0300

    add file that check package versions

---
+++ check.py
@@ -0,0 +1,22 @@
+import torch
+print('torch version: ', torch.__version__)
+
+import lightning as lt
+print('lightning version: ', lt.__version__)
+
+import numpy as np
+print('numpy version: ', np.__version__)
+
+import kornia
+print('kornia version: ', kornia.__version__)
+
+import skorch
+print('skorch version: ', skorch.__version__)
+
+import cv2
+print('opencv version: ', cv2.__version__)
+
+import tensorflow as tf
+print('tensorflow version: ', tf.__version__)
+
+
