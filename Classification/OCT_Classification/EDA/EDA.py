c99843b3a75fd3d2b04dd43eb094252076a20c43
niminim <nimroddaniel1986@gmail.com>
2023-11-28 17:54:01 +0200

    EDA update

--- Classification/OCT_Classification/EDA/EDA.py
+++ Classification/OCT_Classification/EDA/EDA.py
@@ -19,87 +19,21 @@ list_dirs = os.listdir(data_dir)
 print(list_dirs)
 
 d = get_train_test_class_dist(data_dir)
-print_dict_of_count_files_in_sudirectories(d)
+print_dict_of_count_files_in_subdirectories(d)
 plot_train_test_class_dist(d)
 
 
 
 
-
-######
-from PIL import Image, ImageDraw
-
-# Open an image
-im = Image.open(img_p1)
-
-# Create a drawing object
-draw = ImageDraw.Draw(im)
-
-# Draw a rectangle
-draw.rectangle([50, 50, 100, 100], outline="red", width=2)
-
-# Draw text
-draw.text((10, 10), "Hello, PIL!", fill="blue")
-
-# Save or display the modified image
-im.show()
-
 ########################
-import os
-from PIL import Image
-
-def calculate_image_shape_distribution(data_dir):
-    # Get the list of folders in the data directory (e.g., 'train' and 'test')
-    folders = [folder for folder in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, folder))]
-
-    # Initialize a dictionary to store the distribution
-    shape_distribution = {}
-    shape_count = {}
-
-    for folder in folders:
-        print(f'folder: {folder}')
-        folder_path = os.path.join(data_dir, folder)
-
-        # Get the list of subfolders in the current folder (e.g., 'A', 'B', 'C', 'D')
-        subfolders = [subfolder for subfolder in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, subfolder))]
-
-        # Initialize a dictionary for the current folder
-        shape_distribution[folder] = {}
-        shape_count[folder] = {}
-
-        for subfolder in subfolders:
-            print(f'subfolder: {subfolder}')
-            shape_distribution[folder][subfolder] = {}
-            shape_count[folder][subfolder] = {}
-
-            subfolder_path = os.path.join(folder_path, subfolder)
-
-            # Get the list of image files in the current subfolder
-            image_files = [image for image in os.listdir(subfolder_path) if image.lower().endswith(('.png', '.jpg', '.jpeg'))]
-
-            # Calculate the distribution of image shapes
-            shapes = [Image.open(os.path.join(subfolder_path, image)).size for image in image_files]
-            shape_distribution[folder][subfolder] = {shape: shapes.count(shape) for shape in set(shapes)}
-
-            # Accumulate counts in the overall distribution for the current dataset (train or test)
-            for shape in set(shapes):
-                name_shape_name = f'W_{shape[0]}_H_{shape[1]}'
-                print('create new - ', name_shape_name)
-                shape_count[folder][subfolder][name_shape_name] = {}
-                if shape in shape_distribution[folder][subfolder].keys():
-                    shape_count[folder][subfolder][name_shape_name] = shape_distribution[folder][subfolder][shape]
-                else:
-                    shape_count[folder][subfolder][name_shape_name] = 0
-
-    return shape_distribution, shape_count
-
-
 # Example usage
 data_dir = '/home/nim//Downloads/OCT_and_X-ray/OCT2017'
-distribution, shape_count = calculate_image_shape_distribution(data_dir)
+res_dist, res_str_dist = calculate_image_shape_distribution(data_dir)
+plot_image_res_all_classes(res_str_dist, phase='train')
+plot_image_res_all_classes(res_str_dist, phase='test')
 
 # Print the distribution
-for folder, subfolders in distribution.items():
+for folder, subfolders in res_dist.items():
     print(f"\n{folder} Folder:")
     for subfolder, shapes in subfolders.items():
         print(f"  {subfolder} Subfolder:")
--- Classification/OCT_Classification/EDA/EDA_helper.py
+++ Classification/OCT_Classification/EDA/EDA_helper.py
@@ -22,7 +22,7 @@ def get_train_test_class_dist(data_dir):
                 d[phase][label] += 1
     return d
 
-def print_dict_of_count_files_in_sudirectories(dict):
+def print_dict_of_count_files_in_subdirectories(dict):
 
     print('********')
     print('Print the number of images in each folder:')
@@ -33,7 +33,7 @@ def print_dict_of_count_files_in_sudirectories(dict):
             print(f'class: {sub_k}, count: {sub_v}')
     print('********')
 
-############## Plot the Class
+############## Plot the class-distribution
 def plot_class_dist(ax, categories, frequencies, title, ylabel):
     total = sum(frequencies)
     ax.bar(categories, frequencies)
@@ -64,13 +64,108 @@ def plot_train_test_class_dist(d):
         plot_class_dist(ax, categories, frequencies, f'Class Distribution - {key}', 'Frequencies')
 
     fig.suptitle('Class Distribution - Train and Test')  # Title for the entire figure
-    fig.savefig('//home/nim/venv/DL-code/Classification/OCT_Classification/data_dist_train_test.jpg')
+    fig.savefig(f'//home/nim/venv/DL-code/Classification/OCT_Classification/EDA/plots/class_dist_train_test.jpg')
     print('Image saved successfully!')
+######################
 
 
-# labels, data = list(d['train'].keys()), list(d['train'].values())
-# fig = plt.figure(figsize = (9,6))
-# ax = fig.add_subplot(111)
-# ax.plot(labels, data)
-# plt.savefig('//home/nim/venv/DL-code/Classification/OCT_Classification/plot.jpg')
+# Calculate the image-size distribution
+def calculate_image_shape_distribution(data_dir):
+    # Get the list of folders in the data directory (e.g., 'train' and 'test')
+    phases = [phase for phase in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, phase))]
+
+    # Initialize a dictionary to store the distribution
+    res_dist = {} # resolution is presented as tuple
+    res_str_dist = {} # resolution is presented as a string
+
+    for phase in phases:
+        print(f'phase: {phase}')
+        phase_path = os.path.join(data_dir, phase)
+
+        # Get the list of subfolders in the current class (e.g., 'A', 'B', 'C', 'D')
+        classes = [class_name for class_name in os.listdir(phase_path) if os.path.isdir(os.path.join(phase_path, class_name))]
+
+        # Initialize a dictionary for the current phase
+        res_dist[phase] = {}
+        res_str_dist[phase] = {}
+
+        for class_name in classes:
+            print(f'class_name: {class_name}')
+            res_dist[phase][class_name] = {}
+            res_str_dist[phase][class_name] = {}
+
+            class_path = os.path.join(phase_path, class_name)
+
+            # Get the list of image files in the current class
+            image_files = [image for image in os.listdir(class_path) if image.lower().endswith(('.png', '.jpg', '.jpeg'))]
+
+            # Calculate the distribution of image shapes
+            resolutions = [Image.open(os.path.join(class_path, image)).size for image in image_files]
+            res_dist[phase][class_name] = {res: resolutions.count(res) for res in set(resolutions)}
+
+            # Accumulate counts in the overall distribution for the current dataset (train or test)
+            for res in set(resolutions):
+                res_name = f'W_{res[0]}_H_{res[1]}'
+                print('create new - ', res_name)
+                res_str_dist[phase][class_name][res_name] = {}
+                if res in res_dist[phase][class_name].keys():
+                    res_str_dist[phase][class_name][res_name] = res_dist[phase][class_name][res]
+                else:
+                    res_str_dist[phase][class_name][res_name] = 0
+
+    return res_dist, res_str_dist
+
+
+# and plot it (for a specific phase)
+def plot_image_res_all_classes(res_str_dist, phase):
+    assert phase in ['train', 'test']
+    classes = list(res_str_dist[phase].keys())
+
+    # Create subplots for each class
+    fig, axes = plt.subplots(2, 2, figsize=(12, 10))
+    axes = axes.flatten()
+
+    for i, class_name in enumerate(classes):
+        resolutions = list(res_str_dist[phase][class_name].keys())
+        ax = axes[i]
+
+        # Create a bar plot for each subfolder
+        for res in resolutions:
+            shapes = list(res_str_dist[phase][class_name].keys())
+            counts = list(res_str_dist[phase][class_name].values())
+
+            ax.bar(shapes, counts, alpha=0.7, label=f"Resolution: {res}")
+            ax.legend(resolutions)
+
+        ax.set_title(f"{phase.upper()} Image Size Distribution - Class: {class_name}")
+        ax.set_xlabel("Image Size")
+        ax.set_ylabel("Counts")
+
+    plt.tight_layout()
+    plt.show()
+    fig.savefig(f'/home/nim/venv/DL-code/Classification/OCT_Classification/EDA/plots/{phase}_image_size_dist.jpg')
+
+##
+def get_all_resolutions(res_str_dist):
+    classes = list(res_str_dist['train'].keys())
+
+    all_resolutions = set()
+    for class_name in classes:
+        cur_resolutions = list(res_str_dist['train'][class_name].keys())
+        all_resolutions.update(cur_resolutions)
+    return all_resolutions
+
+
+def update_resolutions(res_str_dist, all_resolutions):
+    classes = list(res_str_dist['train'].keys())
+
+    for class_name in classes:
+        for res in all_resolutions:
+            if res not in res_str_dist['train'][class_name].keys():
+                res_str_dist['train'][class_name][res] = 0
+
+    return res_str_dist
+# all_resolutions = get_all_resolutions(res_str_dist)
+# res_str_dist = update_resolutions(res_str_dist, all_resolutions)
+####################
 
